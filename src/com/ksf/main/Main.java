3ab904c40e6c1d95d108ee3bb04f93e76cd8f548
KMAit <kam.ait.ahmed@gmail.com>
2018-03-13 15:41:36 +0100

    Lecture des fichiers

--- src/com/ksf/main/Main.java
+++ src/com/ksf/main/Main.java
@@ -1,5 +1,7 @@
 package com.ksf.main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,13 +9,30 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static javax.script.ScriptEngine.FILENAME;
+
 public class Main {
 
     public static void main(String[] args) {
         String directory = args[0];
         try {
             List<Path> list = Files.find(Paths.get(directory),999,(p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
-            list.forEach(System.out::println);
+            //list.forEach(System.out::println);
+
+            BufferedReader br = null;
+            FileReader fr = null;
+
+            for (Path temp : list) {
+                fr = new FileReader(String.valueOf(temp));
+                br = new BufferedReader(fr);
+                String sCurrentLine;
+
+                while ((sCurrentLine = br.readLine()) != null) {
+                    System.out.println(sCurrentLine);
+                }
+            }
+
+            //br = new BufferedReader(new FileReader(FILENAME));
         } catch (IOException e) {
             throw new Error(e.getMessage());
         }
