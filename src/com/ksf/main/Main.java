b425f76af5786a3df255a48318d8e315fbd4b06d
KMAit <kam.ait.ahmed@gmail.com>
2018-03-15 10:34:55 +0100

    Lecture des mails

--- src/com/ksf/main/Main.java
+++ src/com/ksf/main/Main.java
@@ -6,18 +6,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static javax.script.ScriptEngine.FILENAME;
 
 public class Main {
-
     public static void main(String[] args) {
         String directory = args[0];
         try {
             List<Path> list = Files.find(Paths.get(directory),999,(p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
             //list.forEach(System.out::println);
+
+            List<String> mail = new ArrayList<>();
 
             BufferedReader br = null;
             FileReader fr = null;
@@ -28,11 +33,24 @@ public class Main {
                 String sCurrentLine;
 
                 while ((sCurrentLine = br.readLine()) != null) {
+                    Matcher m = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+").matcher(sCurrentLine);
+                    while (m.find()) {
+                        if(sCurrentLine.contains("To")){
+                            if(!Arrays.asList(mail).contains(m.group())){
+                                System.out.println(m.group());
+                                mail.add(m.group());
+                            }
+                        }
+                    }
+/*
                     System.out.println(sCurrentLine);
+*/
                 }
-            }
 
-            //br = new BufferedReader(new FileReader(FILENAME));
+//                System.out.println(mail.toString());
+            }
+            br.close();
+            fr.close();
         } catch (IOException e) {
             throw new Error(e.getMessage());
         }
